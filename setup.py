9b240030b73ddd2e634f467c0ac6898ac107a498
Ben Kettle <theyellowtoaster@gmail.com>
2020-09-17 01:05:01 -0400

    attempt at creating script

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="code-listing-generator", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="Ben Kettle",
     author_email="",
     description="Creates a LaTeX document with code from all files in the current directory",
     url="https://github.com/bkettle/code-listing-generator",
     packages=setuptools.find_packages(),
+    scripts=['scripts/generate-code-listing'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
